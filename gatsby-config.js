c5dd4310eff2cbe532c9f3ed1690ed04f5d923b0
Takashi Iwamoto <hello@iwamot.com>
2021-08-16 17:51:09 +0900

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Takashi Iwamoto`,
+      summary: `who lives and works in Tokyo building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `iwamot`,
     },
   },
   plugins: [
